a0dcfe1ca35356eb9d311270186ddaa9bdacbabc
sankalp khavale <107344599+sankalpkhavale@users.noreply.github.com>
2022-12-17 09:50:15 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,21 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\monya\OneDrive\Desktop\New folder\programming languaga\all python backup\python save\project python\choice game.py
+ Hello, Welcome to Choice game
+ Instruction : 
+ Python will genrate random number between [1,20] 
+ You will get 3 chances to guess that random number 
+ Best of LUCK!!! 
+ Are you ready, press 1: 
+ To exit, press 2 : 1
+ Game start!!!!
+ This is your 1 chance, Please enter your number : 10
+ ***this number is greater, try again :( ***
+ This is your 2 chance, Please enter your number : 5
+ ***this number is greater, try again :( ***
+ This is your 3 chance, Please enter your number : 2
+ ***this number is greater, try again :( ***
+ YOu used all of your chances, The correct number is 1.
+ You lost bruh!!
+>>> 
